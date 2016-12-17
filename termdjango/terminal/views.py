4f81050f91728cdcff320244b2f837ac1d5a7660
shepherd4 <chebotkines@gmail.com>
2016-12-17 18:54:01 +0300

    add music theme; add new font;

--- termdjango/terminal/views.py
+++ termdjango/terminal/views.py
@@ -18,52 +18,52 @@ def event_cmd_json(request):
 
 			if data['cmd'] == 'menu':
 				jsonResponse['menu'] = []
-				jsonResponse['menu'].append({"value":"<b>menu:</b>\
-					<pre>	menu 			- This message</pre>\
-					<pre>	about 			- Summary of me</pre>\
-					<pre>	projects 		- </pre>\
-					<pre>	skills 			- What I can do</pre>\
-					<pre>	github 			- </pre>\
-					<pre>	contacts 		- Contact me</pre>\
-					<pre>	credits 		- Credits for this website</pre>"})
+				jsonResponse['menu'].append({"value":"<b class='out-label'>menu:</b>\
+					<pre class='out-text'>	menu 			- This message</pre>\
+					<pre class='out-text'>	about 			- Summary of me</pre>\
+					<pre class='out-text'>	projects 		- </pre>\
+					<pre class='out-text'>	skills 			- What I can do</pre>\
+					<pre class='out-text'>	github 			- </pre>\
+					<pre class='out-text'>	contacts 		- Contact me</pre>\
+					<pre class='out-text'>	credits 		- Credits for this website</pre>"})
 
 			if data['cmd'] == 'about':
 				jsonResponse['about'] = []
-				jsonResponse['about'].append({"value":"<b>about</b>\
-					<pre>	Name: 			Evgeniy Chebotkin</pre>\
-					<pre>	DOB: 			20.02.1989</pre>\
-					<pre>	Location: 		Saratov, Russian Federation</pre>"})
+				jsonResponse['about'].append({"value":"<b class='out-label'>about</b>\
+					<pre class='out-text'>	Name: 			Evgeniy Chebotkin</pre>\
+					<pre class='out-text'>	DOB: 			20.02.1989</pre>\
+					<pre class='out-text'>	Location: 		Saratov, Russian Federation</pre>"})
 
 
 
 			if data['cmd'] == 'projects':
 				jsonResponse['projects'] = []
-				jsonResponse['projects'].append({"label":"<b>projects:</b>", "value":""})
+				jsonResponse['projects'].append({"label":"<b class='out-label'>projects:</b>", "value":""})
 
 			if data['cmd'] == 'skills':
 				jsonResponse['skills'] = []
-				jsonResponse['skills'].append({"value":"<b>skills:</b>\
-												<pre>	c, c++, Qt, python, JS, HTML, CSS, Linux</pre>"})				
+				jsonResponse['skills'].append({"value":"<b class='out-label'>skills:</b>\
+												<pre class='out-text'>	c, c++, Qt, python, JS, HTML, CSS, Linux</pre>"})				
 
 
 			if data['cmd'] == 'github':
 				jsonResponse['github'] = []
-				jsonResponse['github'].append({"value":"<b>github:</b>\
-												<pre>	<a href='http://www.github.com/shepherd4'\
+				jsonResponse['github'].append({"value":"<b class='out-label'>github:</b>\
+												<pre class='out-text'>	<a href='http://www.github.com/shepherd4'\
 												style='color: white'>http://www.github.com/shepherd4</a></pre>"})
 
 			if data['cmd'] == 'contacts':
 				jsonResponse['contacts'] = []
-				jsonResponse['contacts'].append({"value":"<b>contacts:</b>\
-					<pre>	email:			chebotkines@gmail.com</pre>"})
+				jsonResponse['contacts'].append({"value":"<b class='out-label'>contacts:</b>\
+					<pre class='out-text'>	email:			chebotkines@gmail.com</pre>"})
 
 			if data['cmd'] == 'credits':
 				jsonResponse['credits'] = []
-				jsonResponse['credits'].append({"value":"<b>credits:</b>\
-						<pre>	Site build by Chebotkin E.S.</pre>\
-						<pre>	Using:</pre>\
-						<pre>	 * Jquery Terminal Emulator by Jakub Jankiewicz: <a href='http://terminal.jcubic.pl' style='color: white'>http://terminal.jcubic.pl</a></pre>\
-						<pre>	 * Django1.8:</pre>\
+				jsonResponse['credits'].append({"value":"<b class='out-label'>credits:</b>\
+						<pre class='out-text'>	Site build by Chebotkin E.S.</pre>\
+						<pre class='out-text'>	Using:</pre>\
+						<pre class='out-text'>	 * Jquery Terminal Emulator by Jakub Jankiewicz: <a href='http://terminal.jcubic.pl' style='color: white'>http://terminal.jcubic.pl</a></pre>\
+						<pre class='out-text'>	 * Django1.8:</pre>\
 						"})				
 	
 	print(jsonResponse)
