7249c2e12fdddd243e1bdb16d26101b22c6d04b5
chebotkines <chebotkines@gmail.com>
2016-12-20 00:57:29 +0000

    v.001

--- termdjango/termdjango/settings.py
+++ termdjango/termdjango/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vb@3avuv$x+q(8#&93cw*#r97sg75c6sjxnqog(7@m42ox*2w&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chebotkines.pythonanywhere.com',]
 
 
 # Application definition
